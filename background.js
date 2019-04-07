c2ec970cf2a0478671f8aab70a4fcdc2d06c11ff
Daniel Abbott <danielabbott@daniel-abbott.com>
2019-04-06 20:34:39 -0600

    Added activeTab permission, color change works - Small formatting changes

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({color:'#3aa757'}, () => {
+  chrome.storage.sync.set({ color:'#3aa757' }, () => {
     console.log("The color is green.")
   });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
+        pageUrl: { hostEquals: 'developer.chrome.com' },
       })
     ],
       actions: [new chrome.declarativeContent.ShowPageAction()]
